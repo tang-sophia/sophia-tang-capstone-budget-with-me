1316ec9a9c322756690dcf0503aee63b5305f77a
Sophia Tang <tang.sophia.st@gmail.com>
2025-02-19 14:15:58 -0600

    add refresh circle beside title with styling

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,13 @@
 import "../Header/Header.scss";
 import React from "react";
-import { Box, Typography } from "@mui/material";
+import { Box, Typography, IconButton } from "@mui/material";
+import RefreshIcon from "@mui/icons-material/Refresh";
 
 const Header = ({ title, subtitle }) => {
+  const handleRefresh = () => {
+    window.location.reload();
+  };
+
   return (
     <Box
       display="flex"
@@ -13,7 +18,15 @@ const Header = ({ title, subtitle }) => {
       width="100%"
       padding="20px 0"
     >
-      <Typography variant="h1">{title}</Typography>
+      <Box display="flex" alignItems="center" gap={1}>
+        <Typography variant="h1">{title}</Typography>
+        <IconButton
+          onClick={handleRefresh}
+          sx={{ color: "primary.main", "&:hover": { color: "secondary.main" } }}
+        >
+          <RefreshIcon fontSize="small" />
+        </IconButton>
+      </Box>
       <Typography variant="h4" className="calendar__subtitle">
         {subtitle}
       </Typography>
